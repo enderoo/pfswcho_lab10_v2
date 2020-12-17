8212303dfe68b94b2a54eab55077a5a4dbdec7a0
enderoo <73643974+enderoo@users.noreply.github.com>
2020-12-17 14:19:47 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
 		while(true){
 			System.out.println("Version 2");
-			String input = scanner.nextLine();
 		}
 	}
 }
